759ac2f8fc96b70350f3efdbfa0375f80a3824b6
omvirgyan <omvirgyan@gmail.com>
2025-07-06 21:13:08 +0530

    Backend: Implement login and signup with register route

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -29,12 +29,25 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: ""
     },
-    instagram: { type: String, default: "" },
-    linkedin: { type: String, default: "" },
-    github: { type: String, default: "" },
-    facebook: { type: String, default: "" },
-
-
-}, { timestamps: true })
+    instagram: {
+        type: String,
+        default: ""
+    },
+    linkedin: {
+        type: String,
+        default: ""
+    },
+    github: {
+        type: String,
+        default: ""
+    },
+    facebook: {
+        type: String,
+        default: ""
+    },
+},
+    {
+        timestamps: true
+    })
 
 export const User = mongoose.model("User", userSchema) 
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./database/db.js"
+import userRoute from "./routes/user.route.js"
 
-const port=3000;
-
-
+dotenv.config()
 const app = express()
-
+//default middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 const PORT = process.env.PORT || 3000
-
+app.use("/api/v1/user",userRoute)
+//http://localhost:8000/api/v1/user/register
 
 
 
 
 app.listen(PORT, ()=>{
     console.log(`Server listen at port ${PORT}`);
-    // connectDB()
+    connectDB()
 })
